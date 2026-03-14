24f4e999d5aa3f25340cee3f011c914266cb5e73
ihzaikrm <ihzaikr@gmail>
2026-03-14 17:23:45 +0800

    fix: BOM + syntax error news cache

---
+++ check_indent.py
@@ -0,0 +1,15 @@
+﻿c = open('bot.py', encoding='utf-8').read()
+lines = c.split('\n')
+for i,l in enumerate(lines):
+    if 'import json as _json' in l:
+        print(i, repr(l))
+    if '_cache = _os.path' in l:
+        print(i, repr(l))
+    if '_news = _json' in l:
+        print(i, repr(l))
+    if '_titles = ' in l:
+        print(i, repr(l))
+    if 'news_text_for_narr = " ".join(_titles)' in l:
+        print(i, repr(l))
+    if "except:" in l and i > 270 and i < 290:
+        print(i, repr(l))
