baee5b78c10b2745a5688a1f5bdd625e8d5317b8
ihzaikrm <ihzaikr@gmail>
2026-03-16 03:38:47 +0800

    cleanup: hapus file debug sementara

--- debug_screener.py
+++
@@ -1,7 +0,0 @@
-﻿with open("bot.py","r",encoding="utf-8") as f: c=f.read()
-
-# Bug: candidates adalah list of dict, tapi ada akses salah
-# Cek isi fungsi build_dynamic_assets
-start = c.find("def build_dynamic_assets")
-end = c.find("\ndef ", start+1)
-print(c[start:end])
