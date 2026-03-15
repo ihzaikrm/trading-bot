e59591ae902b84b2456e913bb3fb1ab7c27caff7
ihzaikrm <ihzaikr@gmail>
2026-03-16 03:52:19 +0800

    fix: market_context dict to str di narrative_scanner

---
+++ fix_narr3.py
@@ -0,0 +1,8 @@
+﻿with open("core/narrative_scanner.py","r",encoding="utf-8") as f: c=f.read()
+
+old = '"MARKET DATA:\\n" + market_context + "\\n\\n"'
+new = '"MARKET DATA:\\n" + (str(market_context) if not isinstance(market_context,str) else market_context) + "\\n\\n"'
+
+c = c.replace(old, new, 1)
+with open("core/narrative_scanner.py","w",encoding="utf-8") as f: f.write(c)
+print("Done!")
