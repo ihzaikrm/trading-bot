edf9f25aea4d15ad130160a861fa7784cd06a5c3
ihzaikrm <ihzaikr@gmail>
2026-03-16 03:58:05 +0800

    cleanup

--- test_llm.py
+++
@@ -1,9 +0,0 @@
-﻿import asyncio
-from core.llm_clients import call_llm
-
-async def test():
-    result = await call_llm("qwen", "Kamu adalah analis narasi market.",
-        "Pilih TOP 3 narasi dari: INFLATION_HEDGE, RISK_OFF, CRYPTO_BULL. Balas HANYA JSON: {\"narratives\":[\"N1\",\"N2\",\"N3\"],\"risk_profile\":\"moderate\",\"urgency\":\"low\"}")
-    print("Result:", repr(result[:300]))
-
-asyncio.run(test())
